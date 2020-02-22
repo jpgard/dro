77cca3050c5bdd564eae4859d6e95c89aa6af6b7
jpgard <joshua.patrick.gardner@gmail.com>
2020-02-21 17:23:18 -0800

    add flag for shuffling

--- dro/datasets.py
+++ dro/datasets.py
@@ -5,7 +5,7 @@ Utilities for generating and working with datasets
 import numpy as np
 
 
-def generate_simulated_dataset(n=1e6, p=0.5):
+def generate_simulated_dataset(n=1e6, p=0.5, shuffle=True):
     """
     Generate a simulated dataset of n examples with proportion p coming from the
     positive class.
@@ -13,11 +13,13 @@ def generate_simulated_dataset(n=1e6, p=0.5):
     n_pos = int(n * p)
     n_neg = n - n_pos
     # The weights and means are set such that the compnents are \sigma apart
-    X_pos = np.random.multivariate_normal(mean=(0.5,0.5), cov=np.eye(2), size=n_pos)
+    X_pos = np.random.multivariate_normal(mean=(0.5, 0.5), cov=np.eye(2), size=n_pos)
     y_pos = np.ones(n_pos)
-    X_neg = np.random.multivariate_normal(mean=(-0.5,0.5), cov=np.eye(2), size=n_neg)
+    X_neg = np.random.multivariate_normal(mean=(-0.5, 0.5), cov=np.eye(2), size=n_neg)
     y_neg = np.zeros(n_neg)
     X = np.concatenate([X_pos, X_neg], axis=0)
     y = np.concatenate([y_pos, y_neg], axis=0)
-    p = np.random.permutation(n)
-    return X[p], y[p]
+    if shuffle:
+        p = np.random.permutation(n)
+        X, y = X[p], y[p]
+    return X, y

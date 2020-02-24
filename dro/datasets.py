9709837a6329108a2ea2e8ce3f2d4218bc576709
jpgard <joshua.patrick.gardner@gmail.com>
2020-02-23 21:21:48 -0800

    type checking; coerce n to integer

--- dro/datasets.py
+++ dro/datasets.py
@@ -5,13 +5,14 @@ Utilities for generating and working with datasets
 import numpy as np
 
 
-def generate_simulated_dataset(n=1e6, p=0.5, shuffle=True):
+def generate_simulated_dataset(n: int = 10 ** 6, p: float = 0.5, shuffle=True):
     """
     Generate a simulated dataset of n examples with proportion p coming from the
     positive class.
     """
+    n = int(n)
     n_pos = int(n * p)
-    n_neg = n - n_pos
+    n_neg = int(n) - n_pos
     # The weights and means are set such that the compnents are \sigma apart
     X_pos = np.random.multivariate_normal(mean=(0.5, 0.5), cov=np.eye(2), size=n_pos)
     y_pos = np.ones(n_pos)

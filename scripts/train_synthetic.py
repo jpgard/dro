4eaffbac4ee0eb4f54c618fc476fb06f4be3eb78
jpgard <joshua.patrick.gardner@gmail.com>
2020-02-24 17:04:57 -0800

    refactor duplicated code into new run_simulation_experiment() function; this eliminates duplicated code in the experimentation process.

--- scripts/train_synthetic.py
+++ scripts/train_synthetic.py
@@ -3,10 +3,12 @@ Train a simple classifier on simulated data.
 """
 
 import numpy as np
+import os
+from functools import partial
 
+import pandas as pd
 import keras
 from keras import backend
-from keras.models import load_model
 from keras.backend import manual_variable_initialization
 import tensorflow as tf
 from tensorflow.python.platform import app
@@ -16,16 +18,21 @@ from dro.sinha.attacks import WassersteinRobustMethod
 from dro.sinha.utils_tf import model_train, model_eval
 from dro.datasets import generate_simulated_dataset
 from dro import keys
+from dro.training.models import logistic_regression_model
 
 FLAGS = flags.FLAGS
 
+# training parameters
 flags.DEFINE_integer('nb_epochs', 5, 'Number of epochs to train model')
 flags.DEFINE_integer('batch_size', 256, 'Size of training batches')
 flags.DEFINE_float('learning_rate', 0.01, 'Learning rate for training')
+
+# simulation parameters
 flags.DEFINE_integer('num_samples', 10 ** 6, 'Number of samples to use in simulation')
 flags.DEFINE_multi_float('pos_prob', [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5],
                          'Probability of positive class in simulated data')
 flags.DEFINE_string('train_dir', './training-logs', 'Training directory')
+flags.DEFINE_string('metrics_dir', './metrics', 'Metrics directory')
 flags.DEFINE_string('filename_erm', 'erm.h5', 'Training directory')
 flags.DEFINE_string('filename_wrm', 'wrm.h5', 'Training directory')
 
@@ -54,6 +61,39 @@ def model_eval_fn(sess, x, y, predictions, predictions_adv, X_test, Y_test, eval
     return metrics_dict
 
 
+def run_simulation_experiment(n, p, sess, adversarial_training=False, save_model=False):
+    """Run a simulation experiment with the specified parameters."""
+    # Generate training and test data; note that the expected accuracy of the optimal
+    # linear classifier on this data is approximately 0.6915.
+    X_train, Y_train = generate_simulated_dataset(n, p)
+    X_test, Y_test = generate_simulated_dataset(n, p)
+
+    # Define input TF placeholder
+    x = tf.placeholder(tf.float32, shape=(None, 2))
+    y = tf.placeholder(tf.float32, shape=(None, 2))
+    # TODO: tune these parameters and conduct a sensitivity analysis.
+    wrm_params = {'eps': 1.3, 'ord': 2, 'y': y, 'steps': 15}
+    # Define the TensorFlow graph.
+    model = logistic_regression_model(n_features=2, n_outputs=2)
+    predictions = model(x)
+    wrm = WassersteinRobustMethod(model, sess=sess)
+    predictions_adv_wrm = model(wrm.generate(x, **wrm_params))
+    eval_fn = partial(model_eval_fn, sess, x, y, predictions, predictions_adv_wrm,
+                      X_test, Y_test, eval_params)
+    if adversarial_training:  # use the adversarial output for training
+        model_train_fn = partial(model_train,
+                                 sess, x, y, predictions_adv_wrm, X_train, Y_train,
+                                 evaluate=eval_fn, args=train_params, save=False)
+    else:
+        model_train_fn = partial(model_train,
+                                 sess, x, y, predictions, X_train, Y_train,
+                                 evaluate=eval_fn, args=train_params, save=False)
+    metrics = model_train_fn()
+    if save_model:
+        model.save(FLAGS.train_dir + '/' + FLAGS.filename_wrm)
+    return metrics
+
+
 def main(argv):
     keras.layers.core.K.set_learning_phase(1)
     manual_variable_initialization(True)
@@ -62,52 +102,21 @@ def main(argv):
     sess = tf.Session()
     keras.backend.set_session(sess)
 
-    # Dictionary to store experimental results; {(n,p) : accuracy}
-    results = dict()
+    # Dictionary to store experimental results; {(n,p, is_adversarial) : accuracy}
+    results = list()
     n = FLAGS.num_samples
     for p in FLAGS.pos_prob:
-        # Generate training and test data; note that the expected accuracy of the optimal
-        # linear classifier on this data is approximately 0.6915.
-        X_train, Y_train = generate_simulated_dataset(n, p)
-        X_test, Y_test = generate_simulated_dataset(n, p)
-
-        # Define input TF placeholder
-        x = tf.placeholder(tf.float32, shape=(None, 2))
-        y = tf.placeholder(tf.float32, shape=(None, 2))
-
-        from dro.training.models import logistic_regression_model
-        model = logistic_regression_model(n_features=2, n_outputs=2)
-        predictions = model(x)
-        wrm = WassersteinRobustMethod(model, sess=sess)
-        # TODO: tune these parameters and conduct a sensitivity analysis.
-        wrm_params = {'eps': 1.3, 'ord': 2, 'y': y, 'steps': 15}
-        predictions_adv_wrm = model(wrm.generate(x, **wrm_params))
-
-        from functools import partial
-        evaluate = partial(model_eval_fn, sess, x, y, predictions, predictions_adv_wrm,
-                           X_test, Y_test, eval_params)
-
-        # Train the model
-        metrics = model_train(sess, x, y, predictions, X_train, Y_train,
-                              evaluate=evaluate, args=train_params, save=False)
-        import ipdb;ipdb.set_trace()
-        # model.save(FLAGS.train_dir + '/' + FLAGS.filename_erm)
-
-        print("Repeating the process, using Wasserstein adversarial training")
-        model_adv = logistic_regression_model(n_features=2, n_outputs=2)
-        predictions_adv = model_adv(x)
-        wrm2 = WassersteinRobustMethod(model_adv, sess=sess)
-        predictions_adv_adv_wrm = model_adv(wrm2.generate(x, **wrm_params))
-
-        evaluate_adv = partial(model_eval_fn, sess, x, y, predictions_adv,
-                               predictions_adv_adv_wrm,
-                               X_test, Y_test, eval_params)
-
-        metrics = model_train(sess, x, y, predictions_adv_adv_wrm, X_train, Y_train,
-                    predictions_adv=predictions_adv_adv_wrm, evaluate=evaluate_adv,
-                    args=train_params, save=False)
-        # model_adv.save(FLAGS.train_dir + '/' + FLAGS.filename_wrm)
-        results[(n, p)] = []
+        for is_adversarial in (True, False):
+            metrics = run_simulation_experiment(
+                n, p, sess, adversarial_training=is_adversarial)
+            results.append(
+                (n, p, is_adversarial, metrics[keys.ACC], metrics[keys.ACC_ADV_W])
+            )
+    metrics_df = pd.DataFrame(
+        results,
+        columns=["n", "p", "is_adversarial", keys.ACC, keys.ACC_ADV_W]
+    )
+    metrics_df.to_csv(os.path.join(FLAGS.metrics_dir, "metrics.csv"), index=False)
 
 
 if __name__ == "__main__":

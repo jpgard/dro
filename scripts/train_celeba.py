d8a82a3a1b8f26e781d2421f3cf150df8bf84faf
jpgard <joshua.patrick.gardner@gmail.com>
2020-03-05 10:46:37 -0800

    implement make_dataset() and use it

--- scripts/train_celeba.py
+++ scripts/train_celeba.py
@@ -19,13 +19,15 @@ img_shape = (218, 178, 3)
 target_colname = "Smiling"
 batch_size = 256
 epochs = 5
-n_train = batch_size * 10
-n_test = 100
+n_train = batch_size * 50
+n_val = batch_size * 10
+n_test = batch_size * 5
 
 # Fetch jpg files for training and testing
 img_files = np.sort(os.listdir(img_dir))
 img_files_train = img_files[:n_train]
-img_files_test = img_files[n_train:n_train + n_test]
+img_files_val = img_files[n_train:n_train + n_val]
+img_files_test = img_files[n_train + n_val: n_train + n_val + n_test]
 
 attributes_df = pd.read_csv(attributes_fp, delim_whitespace=True, skiprows=0,
                             header=1).sort_index().replace(-1, 0)
@@ -45,25 +47,27 @@ def load_image_data(img_file_list):
     return img_data
 
 
-# load the images and the attributes
-img_train = load_image_data(img_files_train)
-attr_train = attributes_df.loc[img_files_train, :].values
-labels_train = attributes_df.loc[img_files_train, target_colname].values
-print("img_ary.shape = {}".format(img_train.shape))
+def make_dataset(img_file_list, batch_size=batch_size):
+    # load the images and the attributes
+    img = load_image_data(img_file_list)
+    attr_train = attributes_df.loc[img_file_list, :].values
+    labels = attributes_df.loc[img_file_list, target_colname].values
+    print("loaded image array with shape = {}".format(img.shape))
+    # Build the dataset of images and labels; attributes are not currently used.
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (img, labels)) \
+        .shuffle(1000) \
+        .batch(batch_size) \
+        .repeat()
+    return dataset
 
-img_test = load_image_data(img_files_test)
-print("img_test.shape = {}".format(img_test.shape))
-attr_test = attributes_df.loc[img_files_test, :].values
-labels_test = attributes_df.loc[img_files_test, target_colname].values
-train_dataset = tf.data.Dataset.from_tensor_slices(
-    (img_train, labels_train)) \
-    .shuffle(1000) \
-    .batch(batch_size) \
-    .repeat()
+
+train_dataset = make_dataset(img_files_train)
+val_dataset = make_dataset(img_files_val)
+test_dataset = make_dataset(img_files_test)
 
 # show a visualization of the first few faces
 # plot_faces(img_train)
-
 
 model = facenet_model()
 model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),

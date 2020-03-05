b4b2af69d2b33048dcba5f9913a5ac90aaab6888
jpgard <joshua.patrick.gardner@gmail.com>
2020-03-05 09:57:09 -0800

    name layers, add the first 3 convolutional sets of facenet model

--- dro/training/models.py
+++ dro/training/models.py
@@ -17,13 +17,16 @@ def facenet_model():
 
     # TODO(jpgard): fully replicate facenet model here.
     inputs = tf.keras.Input(shape=(218, 178, 3))
-    x = Conv2D(64, (7, 7), strides=2)(inputs)
-    x = MaxPool2D((3, 3), strides=2)(x)
-    x = tf.nn.local_response_normalization(x)
-    x = Conv2D(64, (1, 1), strides=1)(x)  # conv2a
-    x = Conv2D(64, (3, 3), strides=1)(x)  # conv2
-    x = tf.nn.local_response_normalization(x)
-    x = MaxPool2D((3, 3), strides=2)(x)
+    x = Conv2D(64, (7, 7), strides=2, name='conv1')(inputs)
+    x = MaxPool2D((3, 3), strides=2, name='pool1')(x)
+    x = tf.nn.local_response_normalization(x, name='rnorm1')
+    x = Conv2D(64, (1, 1), strides=1, name='conv2a')(x)
+    x = Conv2D(64, (3, 3), strides=1, name='conv2')(x)
+    x = tf.nn.local_response_normalization(x, name='rnorm2')
+    x = MaxPool2D((3, 3), strides=2, name='pool2')(x)
+    x = Conv2D(192, (1, 1), strides=1, name='conv3a')(x)
+    x = Conv2D(192, (3, 3), strides=1, name='conv3')(x)
+    x = MaxPool2D((3, 3), strides=2, name='pool3')(x)
     x = Flatten()(x)
     x = tf.keras.layers.Dense(4, activation=tf.nn.relu)(x)
     outputs = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)(x)

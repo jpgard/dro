9c7959d2d50ed0aa8bec75bb69a163095718dd63
jpgard <joshua.patrick.gardner@gmail.com>
2020-03-05 10:11:08 -0800

    add padding=SAME to match facenet implementation

--- dro/training/models.py
+++ dro/training/models.py
@@ -15,18 +15,17 @@ def facenet_model():
     # TODO(jpgard): implement this as subclass of model class; see
     # https://www.tensorflow.org/versions/r1.15/api_docs/python/tf/keras/Model
 
-    # TODO(jpgard): fully replicate facenet model here.
     inputs = tf.keras.Input(shape=(218, 178, 3))
-    x = Conv2D(64, (7, 7), strides=2, name='conv1')(inputs)
-    x = MaxPool2D((3, 3), strides=2, name='pool1')(x)
+    x = Conv2D(64, (7, 7), strides=2, padding='SAME', name='conv1')(inputs)
+    x = MaxPool2D((3, 3), strides=2, padding='SAME', name='pool1')(x)
     x = tf.nn.local_response_normalization(x, name='rnorm1')
-    x = Conv2D(64, (1, 1), strides=1, name='conv2a')(x)
-    x = Conv2D(64, (3, 3), strides=1, name='conv2')(x)
+    x = Conv2D(64, (1, 1), strides=1, padding='SAME', name='conv2a')(x)
+    x = Conv2D(192, (3, 3), strides=1, padding='SAME', name='conv2')(x)
     x = tf.nn.local_response_normalization(x, name='rnorm2')
-    x = MaxPool2D((3, 3), strides=2, name='pool2')(x)
-    x = Conv2D(192, (1, 1), strides=1, name='conv3a')(x)
-    x = Conv2D(192, (3, 3), strides=1, name='conv3')(x)
-    x = MaxPool2D((3, 3), strides=2, name='pool3')(x)
+    x = MaxPool2D((3, 3), strides=2, padding='SAME', name='pool2')(x)
+    x = Conv2D(192, (1, 1), strides=1, padding='SAME', name='conv3a')(x)
+    x = Conv2D(384, (3, 3), strides=1, padding='SAME', name='conv3')(x)
+    x = MaxPool2D((3, 3), strides=2, padding='SAME', name='pool3')(x)
     x = Flatten()(x)
     x = tf.keras.layers.Dense(4, activation=tf.nn.relu)(x)
     outputs = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)(x)

d4dc6ffa7911846dd908d20e4875700a4891f465
jpgard <joshua.patrick.gardner@gmail.com>
2020-03-07 14:27:56 -0800

    experimental implementation manually training on batches

--- scripts/train_vggface2.py
+++ scripts/train_vggface2.py
@@ -185,8 +185,19 @@ def main(argv):
                              )
     custom_vgg_model.summary()
     steps_per_epoch = math.floor(1000 / FLAGS.batch_size)
-    custom_vgg_model.fit_generator(train_ds, steps_per_epoch=steps_per_epoch,
-                                   epochs=FLAGS.epochs)
+    # custom_vgg_model.fit_generator(train_ds, steps_per_epoch=steps_per_epoch,
+    #                                epochs=FLAGS.epochs)
+
+    print_metrics_every_n_steps = 10
+    for epoch in range(FLAGS.epochs):
+        print("epoch %s" % epoch)
+        for step in range(steps_per_epoch):
+            batch_x, batch_y = next(iter(train_ds))
+            metrics = custom_vgg_model.train_on_batch(batch_x, batch_y)
+            if step % print_metrics_every_n_steps == 0:
+                print("step %s metrics:" % step)
+                print(metrics)
+
 
 
 if __name__ == "__main__":

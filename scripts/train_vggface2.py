1844a1fe6e1df1d45baef8a2098ce8f4eb5222fb
jpgard <joshua.patrick.gardner@gmail.com>
2020-03-07 14:15:44 -0800

    add tensorboard and CSV callbacks; add parameters for learning rate and dropout rate; create a model uid and use this to name files

--- scripts/train_vggface2.py
+++ scripts/train_vggface2.py
@@ -10,7 +10,8 @@ export CUDA_VISIBLE_DEVICES=$GPU_ID
 
 # run the script
 python scripts/train_vggface2.py \
-    --img_dir /Users/jpgard/Documents/research/vggface2/train_partitioned_by_label/mouth_open
+    --img_dir /Users/jpgard/Documents/research/vggface2/train_partitioned_by_label
+    /mouth_open
 """
 
 import pandas as pd
@@ -22,6 +23,7 @@ from dro.datasets import train_test_val_split
 import re
 import tensorflow as tf
 import os
+import time
 
 tf.compat.v1.enable_eager_execution()
 
@@ -30,6 +32,9 @@ FLAGS = flags.FLAGS
 flags.DEFINE_integer("batch_size", 16, "batch size")
 flags.DEFINE_integer("epochs", 5, "the number of training epochs")
 flags.DEFINE_string("img_dir", None, "directory containing the aligned celeba images")
+flags.DEFINE_float("learning_rate", 0.001, "learning rate to use")
+flags.DEFINE_float("dropout_rate", 0.8, "dropout rate to use in fully-connected layers")
+
 
 # Suppress the annoying tensorflow 1.x deprecation warnings; these make console output
 # impossible to parse.
@@ -37,9 +42,21 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 CLASS_NAMES = np.array(["0", "1"])
 
+
 def main(argv):
     list_ds = tf.data.Dataset.list_files(str(FLAGS.img_dir + '/*/*/*.jpg'), shuffle=True,
                                          seed=2974)
+
+    def make_model_uid():
+        model_uid = """bs{batch_size}e{epochs}lr{lr}dropout{dropout_rate}""".format(
+            batch_size=FLAGS.batch_size,
+            epochs=FLAGS.epochs,
+            lr=FLAGS.learning_rate,
+            dropout_rate=FLAGS.dropout_rate
+        )
+
+    uid = make_model_uid()
+
     # for f in list_ds.take(3):
     #     print(f.numpy())
 
@@ -56,7 +73,7 @@ def main(argv):
         img = tf.image.convert_image_dtype(img, tf.float32)
         # resize to a square image of 256 x 256, then crop to random 224 x 224
         img = tf.expand_dims(img, axis=0)
-        img = tf.image.resize_images(img, size=(256,256), preserve_aspect_ratio=True)
+        img = tf.image.resize_images(img, size=(256, 256), preserve_aspect_ratio=True)
         img = tf.image.resize_with_crop_or_pad(img, target_height=256, target_width=256)
         img = tf.squeeze(img, axis=0)
         img = tf.image.random_crop(img, (224, 224, 3))
@@ -65,9 +82,9 @@ def main(argv):
         # https://github.com/rcmalli/keras-vggface/blob/master/keras_vggface/utils.py ;
         # divide means by 255.0 since the conversion above restricts to range [0,1].
         if normalize_by_channel:
-            ch1mean = tf.constant(91.4953/255.0, shape=(224, 224, 1))
-            ch2mean = tf.constant(103.8827/255.0, shape=(224, 224, 1))
-            ch3mean = tf.constant(131.0912/255.0, shape=(224, 224, 1))
+            ch1mean = tf.constant(91.4953 / 255.0, shape=(224, 224, 1))
+            ch2mean = tf.constant(103.8827 / 255.0, shape=(224, 224, 1))
+            ch3mean = tf.constant(131.0912 / 255.0, shape=(224, 224, 1))
             channel_norm_tensor = tf.concat([ch1mean, ch2mean, ch3mean], axis=2)
             img -= channel_norm_tensor
         return img
@@ -121,6 +138,7 @@ def main(argv):
     from tensorflow.keras.layers import Flatten, Dense, Input, Activation, Dropout
     from tensorflow.keras.metrics import AUC, TruePositives, TrueNegatives, \
         FalsePositives, FalseNegatives
+    from tensorflow.keras.callbacks import TensorBoard, CSVLogger
     from keras_vggface.vggface import VGGFace
     # Convolution Features
     vgg_model = VGGFace(include_top=False, input_shape=(224, 224, 3))
@@ -132,14 +150,28 @@ def main(argv):
     x = Flatten(name='flatten')(last_layer)
     x = Dense(4096, name='fc6')(x)
     x = Activation('relu', name='fc6/relu')(x)
-    x = Dropout(rate=0.8)(x)
+    x = Dropout(rate=FLAGS.dropout_rate)(x)
     x = Dense(256, name='fc7')(x)
     x = Activation('relu', name='fc7/relu')(x)
-    x = Dropout(rate=0.8)(x)
+    x = Dropout(rate=FLAGS.dropout_rate)(x)
     x = Dense(1, name='fc8')(x)
     out = Activation('sigmoid', name='fc8/sigmoid')(x)
 
     custom_vgg_model = Model(vgg_model.input, out)
+
+    tensorboard_callback = TensorBoard(
+        log_dir='./training-logs/{}'.format(uid),
+        batch_size=FLAGS.batch_size,
+        write_graph=True,
+        # write_grads=False,
+        # write_images=False,
+        # embeddings_freq=0,
+        # embeddings_layer_names=None,
+        # embeddings_metadata=None,
+        # embeddings_data=None,
+        update_freq='epoch')
+    csv_callback = CSVLogger("./metrics/{}-vggface2-training.log".format(uid))
+
     custom_vgg_model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01),
                              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
                              metrics=['accuracy',
@@ -148,10 +180,11 @@ def main(argv):
                                       FalsePositives(name='fp'),
                                       TrueNegatives(name='tn'),
                                       FalseNegatives(name='fn')
-                                      ]
+                                      ],
+                             callbacks=[tensorboard_callback, csv_callback]
                              )
     custom_vgg_model.summary()
-    custom_vgg_model.fit_generator(train_ds, steps_per_epoch=1000/FLAGS.batch_size,
+    custom_vgg_model.fit_generator(train_ds, steps_per_epoch=1000 / FLAGS.batch_size,
                                    epochs=FLAGS.epochs)
 
 

42d4001a794e290212dedd75a8cf03fbb26ce79b
jpgard <joshua.patrick.gardner@gmail.com>
2020-03-12 22:18:31 -0700

    implement write_one_crop_to_file() to separate logic into function

--- scripts/extract_vgg_embeddings.py
+++ scripts/extract_vgg_embeddings.py
@@ -21,6 +21,7 @@ import os
 import pandas as pd
 import tensorflow as tf
 from sklearn.metrics.pairwise import cosine_similarity
+import matplotlib.pyplot as plt
 
 from keras_vggface.vggface import VGGFace
 from dro.utils.training_utils import process_path, preprocess_dataset
@@ -46,13 +47,23 @@ flags.DEFINE_integer("batch_size", 16, "batch size to use for inference")
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
+def write_one_crop_to_file(train_ds):
+    x = 0
+    for image in train_ds.take(FLAGS.n_embed):
+        ax = plt.subplot(FLAGS.n_embed // 2, 2, x + 1)
+        ax.imshow(image.numpy())
+        plt.axis('off')
+        x += 1
+    plt.savefig("./debug/crop_sample.png")
+
+
 def main(argv):
     filepattern = str(FLAGS.img_dir + '/*/*/*.jpg')  # TODO(jpgard): allow
     # specification of this
     embedding_dim = 512
     image_ids = glob.glob(filepattern)
     N = len(image_ids)
-    list_ds = tf.data.Dataset.list_files(filepattern, shuffle=True,
+    list_ds = tf.data.Dataset.list_files(filepattern, shuffle=False,
                                          seed=2974)
     from functools import partial
     _process_path = partial(process_path, crop=False)
@@ -68,16 +79,10 @@ def main(argv):
     # Apply different random cropping to each iterate of x, and drop the labels.
     train_ds = train_ds.map(lambda x: random_crop_and_resize(x))
     # Write one image to file, as debug check
-    import matplotlib.pyplot as plt
-    x = 0
-    for image in train_ds.take(FLAGS.n_embed):
-        ax = plt.subplot(FLAGS.n_embed//2, 2, x + 1)
-        ax.imshow(image.numpy())
-        plt.axis('off')
-        x += 1
-    plt.savefig("./debug/crop_sample.png")
+    if FLAGS.n_embed > 1:
+        write_one_crop_to_file(train_ds)
+    # Run the inference
     train_ds = train_ds.batch(FLAGS.batch_size)
-
     vgg_model = VGGFace(include_top=False, input_shape=(224, 224, 3), pooling='avg')
     embeddings = vgg_model.predict_generator(train_ds)
 
@@ -95,6 +100,7 @@ def main(argv):
     # TODO(jpgard): check that these embeddings are CLOSE to embeddings which are
     #  generated with n_embed=1. The averaging should change the embeddings a little
     #  bit, but not too much, and the difference should be random across samples.
+
 
 if __name__ == "__main__":
     app.run(main)

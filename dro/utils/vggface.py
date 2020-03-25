8ddda9d797e4b7ac35e231f29947a39cd5ea9cc6
jpgard <joshua.patrick.gardner@gmail.com>
2020-03-25 15:07:11 -0700

    bug fixes to two functions ported from scripts: remove references to module variables

--- dro/utils/vggface.py
+++ dro/utils/vggface.py
@@ -3,15 +3,13 @@ import re
 
 import pandas as pd
 
-from scripts.partition_vggface2_by_label import FLAGS
-
 
 def image_uid_from_fp(f):
     image_id = re.match('.*(.*/.*/.*\\.jpg)', f)
     if image_id:
         return image_id.group(1)
     else:  # not detected; invalid path
-        print("[WARNING] invalid vgg identifier: {}".format(fp))
+        print("[WARNING] invalid vgg identifier: {}".format(f))
         return None
 
 
@@ -21,7 +19,7 @@ def make_annotations_df(anno_dir):
     for f in os.listdir(anno_dir):
         if (not f.startswith(".")) and f.endswith(".txt"):
             label = re.match("\d+-(.*)\.txt", f).group(1)
-            attributes_df = pd.read_csv(os.path.join(FLAGS.anno_dir, f),
+            attributes_df = pd.read_csv(os.path.join(anno_dir, f),
                                         delimiter="\t",
                                         index_col=0)
             attributes_df.columns = [label, ]

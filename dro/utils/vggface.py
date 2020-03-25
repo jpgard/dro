be70440fe8d2c7a4c32b70631061bdf08066c6cf
jpgard <joshua.patrick.gardner@gmail.com>
2020-03-25 15:17:44 -0700

    fix indexing, which previously consumed the first row of data, and use the FILENAME_COLNAME key to match other modules.

--- dro/utils/vggface.py
+++ dro/utils/vggface.py
@@ -2,6 +2,7 @@ import os
 import re
 
 import pandas as pd
+from dro.keys import FILENAME_COLNAME
 
 
 def image_uid_from_fp(f):
@@ -19,9 +20,11 @@ def make_annotations_df(anno_dir):
     for f in os.listdir(anno_dir):
         if (not f.startswith(".")) and f.endswith(".txt"):
             label = re.match("\d+-(.*)\.txt", f).group(1)
-            attributes_df = pd.read_csv(os.path.join(anno_dir, f),
-                                        delimiter="\t",
-                                        index_col=0)
+            attributes_df = pd.read_csv(
+                os.path.join(anno_dir, f),
+                delimiter="\t",
+                names=[FILENAME_COLNAME, label]) \
+                .set_index(FILENAME_COLNAME)
             attributes_df.columns = [label, ]
             annotations.append(attributes_df)
     return pd.concat(annotations, axis=1)

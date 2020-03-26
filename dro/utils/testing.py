96d8c9151c8662a2783fc2abdf8df8e95b8268f8
jpgard <joshua.patrick.gardner@gmail.com>
2020-03-26 12:07:51 -0700

    implement assert_file_exists

--- dro/utils/testing.py
+++ dro/utils/testing.py
@@ -2,7 +2,10 @@
 Utilities for testing.
 """
 
+import os
+
 import numpy as np
+
 
 def assert_shape_equal(arr1, arr2):
     assert arr1.shape == arr2.shape, "expected identical shapes, got shapes {} {}".format(
@@ -15,4 +18,9 @@ def assert_ndims(arr: np.ndarray, ndims: int):
     assert len(arr.shape) == ndims, "expected array of shape {}; got {}".format(
         ndims, len(arr.shape)
     )
+    return
+
+
+def assert_file_exists(fp):
+    assert os.path.exists(fp), "file {} does not exist.".format(fp)
     return

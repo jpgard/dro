e05113cc12fc39f7565e2f5ec2163fa28ac46fd5
jpgard <joshua.patrick.gardner@gmail.com>
2020-04-04 10:38:22 -0700

    bug fix: must provide a loss to compile the model

--- scripts/evaluate_cleverhans.py
+++ scripts/evaluate_cleverhans.py
@@ -129,7 +129,7 @@ def main(argv):
                                         activation="softmax")
         model_compile_args = {
             "optimizer": tf.keras.optimizers.SGD(learning_rate=FLAGS.learning_rate),
-            "loss": None,  # No loss needed for evaluation.
+            "loss": tf.keras.losses.CategoricalCrossentropy(from_logits=False),
             "metrics": ['accuracy', ]}
         vgg_model_base.compile(**model_compile_args)
         attack = get_attack(FLAGS, vgg_model_base, sess)

bedb26bd45d5500fd3deffd8dbe6f44c487a74bd
jpgard <joshua.patrick.gardner@gmail.com>
2020-04-05 13:44:23 -0700

    deprecate the make_compiled_model(); instead use evaluate_cleverhans_models_on_dataset() to evaluate models in graph mode. First complete and working implementation of evaluate_cleverhans_models_on_dataset() ! :)

--- scripts/evaluate_cleverhans.py
+++ scripts/evaluate_cleverhans.py
@@ -35,7 +35,7 @@ do
 done
 """
 
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 import pprint
 from statistics import mean
 from absl import app, flags
@@ -75,57 +75,45 @@ define_adv_training_flags()
 define_eval_flags()
 
 
-def make_compiled_model(sess, attack_params, is_adversarial: bool, activation="softmax"):
-    model = vggface2_model(dropout_rate=FLAGS.dropout_rate, activation=activation)
-    model(model.input)
-    # Initialize the attack object
-    attack = get_attack(FLAGS, model, sess)
-    print("[INFO] using attack {} with params {}".format(FLAGS.attack, attack_params))
+def evaluate_cleverhans_models_on_dataset(sess: tf.Session, eval_dset, epsilon):
+    """Evaluate the pretrained models (both base and adversarial) on eval_dset.
 
-    adv_acc_metric = get_adversarial_acc_metric(model, attack, attack_params)
-    if is_adversarial:
-        loss = get_adversarial_loss(model, attack, attack_params, FLAGS.adv_multiplier)
-    else:
-        loss = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
-    model_compile_args_base = get_model_compile_args(FLAGS, loss=loss,
-                                                     adv_acc_metric=adv_acc_metric)
-    model.compile(**model_compile_args_base)
-    load_model_weights_from_flags(model, FLAGS, is_adversarial=is_adversarial)
-    return model
+    Both models are evaluated at the same time because the attacks are the same (
+    attacks are performed wrt the base model).
 
+    :return: a tuple of dicts (res, sample_batch) where res is a dict of accuracy
+        metrics, and sample_batch is a dict of elements corresponding to a sample batch
+        (by default, the first batch is used).
+    """
 
-def evaluate_cleverhans_models_on_dataset(sess: tf.Session, eval_dset):
+    # TODO(jpgard): instead of using `epsilon`, take a dict of input arguments which
+    #  will be pased directly to get_attack_params; this would allow flexible
+    #  specificatoin of any attack's parameters, not just FGSM.
+
     # Create an iterator which generates (batch_of_x, batch_of_y) tuples of numpy
     # arrays.
     eval_dset_numpy = tfds.as_numpy(eval_dset)
 
-    attack_params = get_attack_params(FLAGS.adv_step_size)
-
-    vgg_model_base = vggface2_model(dropout_rate=FLAGS.dropout_rate,
-                                    activation="softmax")
-
     # Use the same compile args for both models. Since we are not training,
     # the optimizer and loss will not be used to adjust any parameters.
-
-    model_compile_args = {
-        "optimizer": tf.keras.optimizers.SGD(learning_rate=FLAGS.learning_rate),
-        "loss": tf.keras.losses.CategoricalCrossentropy(from_logits=False),
-        "metrics": ['accuracy', ]}
+    model_init_args = {"dropout_rate": FLAGS.dropout_rate,
+                       "activation": "softmax"}
+    model_compile_args = get_model_compile_args(
+        FLAGS, tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+        adv_acc_metric=None)
+    vgg_model_base = vggface2_model(**model_init_args)
     vgg_model_base.compile(**model_compile_args)
     load_model_weights_from_flags(vgg_model_base, FLAGS, is_adversarial=False)
 
-    vgg_model_adv = vggface2_model(dropout_rate=FLAGS.dropout_rate,
-                                   activation="softmax")
+    vgg_model_adv = vggface2_model(**model_init_args)
     vgg_model_adv.compile(**model_compile_args)
     load_model_weights_from_flags(vgg_model_adv, FLAGS, is_adversarial=True)
 
-    # The attack is always computed relative to the base model
+    # Initialize the attack. The attack is always computed relative to the base model.
+    attack_params = get_attack_params(epsilon)
     attack = get_attack(FLAGS, vgg_model_base, sess)
 
-    accuracies = defaultdict(list)
-
     # Define the ops to run for evaluation
-
     x = tf.compat.v1.placeholder(tf.float32, shape=(None, 224, 224, 3))
     x_perturbed = attack.generate(x, **attack_params)
     y = tf.compat.v1.placeholder(tf.float32, shape=(None, 2))  # [batch_size, 2]
@@ -133,57 +121,76 @@ def evaluate_cleverhans_models_on_dataset(sess: tf.Session, eval_dset):
     yhat_base_clean = vgg_model_base(x)
     yhat_adv_perturbed = vgg_model_adv(x_perturbed)
     yhat_adv_clean = vgg_model_adv(x)
-    # the acc_ results have shape [batch_size,] but note that the final batch of a
-    # dataset may not be full-sized (when N % batch_size != 0)
+
+    # the acc_ results tensors each have shape [batch_size,] but note that the final
+    # batch of a dataset may not be full-sized (when N % batch_size != 0)
 
     acc_bp_op = tf.keras.metrics.categorical_accuracy(y, yhat_base_perturbed)
     acc_bc_op = tf.keras.metrics.categorical_accuracy(y, yhat_base_clean)
     acc_ap_op = tf.keras.metrics.categorical_accuracy(y, yhat_adv_perturbed)
     acc_ac_op = tf.keras.metrics.categorical_accuracy(y, yhat_adv_clean)
-    ops_to_run = [acc_bp_op, acc_bc_op, acc_ap_op, acc_ac_op,
-                  yhat_base_perturbed, yhat_base_clean,
-                  yhat_adv_perturbed, yhat_adv_clean,
-                  x_perturbed]
 
-    # TODO(jpgard): can we read these directly from the op names (probably need to
-    #  assign them names above)?
+    # Build dicts of {op_name: op_tensor} to run in the session.
 
-    op_names = ["acc_base_perturbed", "acc_base_clean", "acc_adv_perturbed",
-                "acc_adv_clean",
-                "yhat_base_perturbed", "yhat_base_clean",
-                "yhat_adv_perturbed", "yhat_adv_clean",
-                "x_perturbed"]
+    # The ops which run on clean data (these are always run even when epsilon=0)
+    clean_data_ops = OrderedDict({"acc_base_clean": acc_bc_op,
+                                  "acc_adv_clean": acc_ac_op,
+                                  "yhat_base_clean": yhat_base_clean,
+                                  "yhat_adv_clean": yhat_adv_clean,
+                                  })
 
-    acc_keys_to_update = ["acc_base_perturbed", "acc_base_clean", "acc_adv_perturbed",
-                          "acc_adv_clean"]
+    # The ops which run on perturbed data (these are only run when epsilon > 0).
+    perturbed_data_ops = OrderedDict({"acc_base_perturbed": acc_bp_op,
+                                      "acc_adv_perturbed": acc_ap_op,
+                                      "yhat_base_perturbed": yhat_base_perturbed,
+                                      "yhat_adv_perturbed": yhat_adv_perturbed,
+                                      "x_perturbed": x_perturbed
+                                      })
+
+    if epsilon > 0:
+        ops_to_run = OrderedDict({**clean_data_ops, **perturbed_data_ops})
+        sample_batch_keys_to_update = ["yhat_base_clean", "yhat_adv_clean",
+                                       "yhat_base_perturbed", "yhat_adv_perturbed",
+                                       "x_perturbed"]
+    else:
+        ops_to_run = clean_data_ops
+        sample_batch_keys_to_update = ["yhat_base_clean", "yhat_adv_clean"]
+
+    # The names of the ops which compute accuracy
+    acc_keys_to_update = [k for k in ops_to_run.keys() if k.startswith("acc")]
+
     batch_index = 0
     sample_batch = defaultdict()
+    accuracies = defaultdict(list)
+
     for batch_x, batch_y in eval_dset_numpy:
-        res = sess.run(ops_to_run, feed_dict={x: batch_x, y: batch_y})
-        res_dict = dict(zip(op_names, res))
+        batch_res = sess.run(list(ops_to_run.values()),
+                             feed_dict={x: batch_x, y: batch_y})
+        batch_res = dict(zip(ops_to_run.keys(), batch_res))
         if batch_index == 0:
             print("[INFO] storing sample batch.")
-            sample_batch["x_clean"] = batch_x
-            sample_batch["x_perturbed"] = res_dict["x_perturbed"]
+            # Always store the x and y
+            sample_batch["x"] = batch_x
             sample_batch["y"] = batch_y
-            sample_batch["yhat_base_perturbed"] = res_dict["yhat_base_perturbed"]
-            sample_batch["yhat_base_clean"] = res_dict["yhat_base_clean"]
-            sample_batch["yhat_adv_perturbed"] = res_dict["yhat_adv_perturbed"]
-            sample_batch["yhat_adv_clean"] = res_dict["yhat_adv_clean"]
+            # Update the remaining sample batch keys.
+            for k in sample_batch_keys_to_update:
+                sample_batch[k] = batch_res[k]
 
         # We store the binary "correct" vector for categorical accuracy as a list;
         # this is because we need to know the exact dataset size to compute overall
         # accuracy.
         for k in acc_keys_to_update:
-            accuracies[k].extend(res_dict[k].tolist())
+            accuracies[k].extend(batch_res[k].tolist())
         # Print stats for debugging
         for k in acc_keys_to_update:
-            print("batch {} {}: {}".format(batch_index, k, mean(res_dict[k])))
+            print("batch {} {}: {}".format(batch_index, k, mean(batch_res[k])))
         batch_index += 1
+        if batch_index > 2:
+            break
 
-    res = zip([(k, mean(accuracies[k])) for k in acc_keys_to_update])
-    import ipdb;ipdb.set_trace()
+    res = {k: mean(accuracies[k]) for k in acc_keys_to_update}
     return res, sample_batch
+
 
 def main(argv):
     # Object used to keep track of (and return) key accuracies
@@ -205,32 +212,19 @@ def main(argv):
     K.set_learning_phase(False)
 
     for attr_val in ("0", "1"):
-        # TODO(jpgard): don't load the model weights and call the model inside the
-        #  adversarial loss function. Instead, just generate pperturbed inputs for each
-        #  batch, and compute the accuracy on those batches as you iterate over them.
-        #  In other words, you never call model(x_adv) inside any function; you only
-        #  call attack.generate() and then use batches of those generated examples to
-        #  compute the accuracy.
-
-        #####################################################################
 
         print("[INFO] reached dev block.")
-        #### Try to load the model without using the adversarial loss metric.
+
         eval_dset = make_pos_and_neg_attr_datasets(FLAGS, write_samples=False)[
             attr_val].dataset
 
-        res = evaluate_cleverhans_models_on_dataset(sess, eval_dset)
+        # Do the evalaution with no epsilon; this only evaluates on the clean data.
+        res, sample_batch = evaluate_cleverhans_models_on_dataset(sess, eval_dset,
+                                                                  epsilon=0.)
         import ipdb;
         ipdb.set_trace()
+        # TODO(jpgard): add the corresponding entries of res to results, below.
 
-        # print(acc)
-        #####################################################################
-        # load the models
-        vgg_model_base = make_compiled_model(sess, attack_params, is_adversarial=False)
-
-        print("[INFO] evaluating base model on clean data")
-        _, acc, _ = vgg_model_base.evaluate(make_pos_and_neg_attr_datasets(FLAGS)[
-                                                attr_val].dataset)
         results.add_result({"metric": keys.ACC,
                             "value": acc,
                             "model": keys.BASE_MODEL,
@@ -241,12 +235,6 @@ def main(argv):
                             "epsilon": None
                             })
 
-        # Adversarial model
-        vgg_model_adv = make_compiled_model(sess, attack_params, is_adversarial=True)
-
-        print("[INFO] evaluating adversarial model on clean data")
-        _, acc, _ = vgg_model_adv.evaluate(
-            make_pos_and_neg_attr_datasets(FLAGS)[attr_val].dataset)
         results.add_result({"metric": keys.ACC,
                             "value": acc,
                             "model": keys.ADV_MODEL,
@@ -257,23 +245,8 @@ def main(argv):
                             "epsilon": None
                             })
 
-        # Remove the models to eliminate their memory footprint
-        del vgg_model_base
-        del vgg_model_adv
-
         for adv_step_size_to_eval in ADV_STEP_SIZE_GRID:
             print("adv_step_size_to_eval %f" % adv_step_size_to_eval)
-            # Build the models for evaluation; we need to build a new model for each
-            # epsilon due to the way the cleverhans objects are constructed.
-            attack_params = get_attack_params(adv_step_size_to_eval)
-            # load the models
-            vgg_model_base = make_compiled_model(sess, attack_params,
-                                                 is_adversarial=False)
-            # Adversarial model
-            vgg_model_adv = make_compiled_model(sess, attack_params, is_adversarial=True)
-
-            _, _, adv_acc = vgg_model_base.evaluate(
-                make_pos_and_neg_attr_datasets(FLAGS)[attr_val].dataset)
 
             results.add_result({"metric": keys.ACC,
                                 "value": adv_acc,

3907c5b9a759e37cffd3c19a1513e2767b3ede81
jpgard <joshua.patrick.gardner@gmail.com>
2020-04-16 15:22:31 -0700

    attacks from staib and jegelka paper

---
+++ dro/staib/__init__.py
@@ -0,0 +1,7 @@
+"""
+Attacks from Distributionally Robust Deep Learning as a
+Generalization of Adversarial Training
+https://machine-learning-and-security.github.io/papers/mlsec17_paper_30.pdf
+
+Code provided by author.
+"""
